51b77b46beba5c678057590d16e3630320705103
Daniel Butler <danielmbutler@hotmail.co.uk>
2021-08-25 15:16:06 +0100

    Initial commit

---
+++ app/src/main/java/com/dbtechprojects/dummyuserapi/ui/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dbtechprojects.dummyuserapi.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dbtechprojects.dummyuserapi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
